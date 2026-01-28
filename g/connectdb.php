77f17110506a75791157414e3df70b1d21012db1
thaanacha-30 <66010914030@msu.ac.th>
2026-01-28 16:21:02 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -2,7 +2,7 @@
 		$host = "localhost";
 		$user = "root";
 		$pwd = "Zx.0966765968";
-		$db = "4029db";
+		$db = "4030db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
 ?>
