7fe082299fcb205994018fb53b68b716956438cc
Thanacha-30 <66010914030@msu.ac.th>
2026-02-17 21:05:29 +0700

    ฟ

--- i/connectdb.php
+++ i/connectdb.php
@@ -2,7 +2,7 @@
 		$host = "localhost";
 		$user = "root";
 		$pwd = "Zx.0966765968";
-		$pwd ="";
+		//$pwd ="";
 		$db = "4030db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
