508916ab2220546c02312dbfb510ce92ced6c7cc
Thanacha-30 <66010914030@msu.ac.th>
2026-02-17 19:37:49 +0700

    a

--- i/a.php
+++ i/a.php
@@ -3,42 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ธนชา พรหมบุตร(ต้นน้ำ)</title>
+    <title>วิทวัส วงศ์ภาคำ(เกม)</title>
 </head>
 <body>
-    <h1>งาน i --ธนชา พรหมบุตร(ต้นน้ำ)</h1>
-    <feom method="post"action="">
-        ชื่อภาค<input type ="text" name="rname" autofocus required>
-        <button type="sumbit" name="submit">บันทึก</button>
-</from><br><br>
-<?php
-if(ISSET($_POST['SUBMIT'])){
-    include_once("connectdb.php");
-    $sql2 = "iINSERT INTO 'regions'('r_id',r_name')VALUES(NULL,
-    '{$rname}')";
-    mysqli_query($conn'$sql2 or die("เพิ่มข้อมูลไม่ได้")
-}
+    <h1>วิทวัส วงศ์ภาคำ(เกม)</h1>
+<form method="post" action="">
+    ชื่อภาค<input type="text" name="rname"aotofocus requiewd>
+    <button type="submit" name="Submit">บันทึก</button>
+/form><br><br>
 
+
+<?phpif(isst($_POST['Submit'])){
+    include_once("connectdb.php");
+    $rname = $_POST['rname'];
+    $sql2 = "INSERT INTO 'regions'('r_id','r_name')VALUES(NULL,'{$rname}')";
+    mysqli_qery($conn,$sql2) or die ("เพิ่มข้อมูลไม่ได้");
+
+}
+?>
 
 <table border="1">
     <tr>
         <th>รหัสภาค</th>
         <th>ชื่อภาค</th>
+        <th>ลบ</th>
+
     </tr>
 <?php
 include_once("connectdb.php");
-$sql = "SELECT * FROM 'regions'";
-$rs = mysqli_qery($conn,$sql);
+$sql = "SELECT * FROM regions ";
+$rs = mysqli_query($conn,$sql);
 
 while($data = mysqli_fetch_array($rs)){
 ?>
     <tr>
         <td><?php echo $data['r_id'];?></td>
         <td><?php echo $data['r_name'];?></td>
-        <tdwidth="80" align="center"><img.src="images/delete.jpg" width="20"></td>
+        <td width="80" align="ceter"> <img src="images/delete.jpg" width="20"></td>
 
     </tr>
-<?php} ?>
+<?php } ?>
 </table> 
 
 </body>
