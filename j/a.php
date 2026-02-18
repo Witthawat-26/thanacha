4b876a810df01b70aac9290805d0d362050b68bc
Thanacha-30 <66010914030@msu.ac.th>
2026-02-18 13:44:38 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ธนชา พรหมบุตร (ต้นน้ำ)</title>
+</head>
+<body>
+    <h1>ธนชา พรหมบุตร (ต้นน้ำ)
+</h1>
+    
+</body>
+</html>
