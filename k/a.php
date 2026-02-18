ab7d6c16aa7da4431195638ee3668826bd3f5a6e
Thanacha-30 <66010914030@msu.ac.th>
2026-02-18 16:59:08 +0700

    a

--- k/a.php
+++ k/a.php
@@ -7,8 +7,8 @@
 </head>
 <body>
     <h1>ธนชา พรหมบุตร(ต้นน้ำ)</h1>
-    <button onclick="showImage('1.jpg', this)" style="background-color:black; color:white; padding:10px; border:none;"><img src="1.jpg" width="150"></button>
-    <button onclick="showImage('1.jpg', this)" style="background-color:black; color:white; padding:10px; border:none;"><img src="1.jpg" width="150"></button>
+    <button onclick="showImage('1.jpg', this)" style= "color:white; padding:10px; border:none;"><img src="1.jpg" width="150"></button>
+    <button onclick="showImage('1.jpg', this)" style= "color:white; padding:10px; border:none;"><img src="1.jpg" width="150"></button>
     
 </body>
 </html>
