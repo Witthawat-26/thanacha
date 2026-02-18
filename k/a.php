f218c5795625fd215d4768a933ba57bd14849216
Thanacha-30 <66010914030@msu.ac.th>
2026-02-18 17:13:52 +0700

    a

--- k/a.php
+++ k/a.php
@@ -13,7 +13,12 @@
 <button onclick="showImage('2.jpg', this)" style="background-color:orange; color:white; padding:10px; border:none;">
     เปิดรูปที่ 2
 </button>
+<script>
 
+function showImage(imgSrc, btn){
+    btn.innerHTML = "<img src='" + imgSrc + "' width='150'>";
+}
+</script>
 
     
 </body>
