de6b3aa7269e615c9317839648ee5354042d0ab7
Thanacha-30 <66010914030@msu.ac.th>
2026-02-18 16:49:35 +0700

    a

--- k/a.php
+++ k/a.php
@@ -7,8 +7,8 @@
 </head>
 <body>
     <h1>ธนชา พรหมบุตร(ต้นน้ำ)</h1>
-    <button type="submit" name="Submit">ตัวเอง</button>
-    <a href="images/11.jpg"><img src="images/11.jpg">
+    <button type="submit" name="Submit">ตัวเอง<a href="button"><img src="images/11.jpg"></button>
+    
     
 </body>
 </html>
