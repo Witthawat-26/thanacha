4c6ede8053ce55eba0ad206f97e3089c2381064f
Thanacha-30 <66010914030@msu.ac.th>
2026-02-18 17:01:56 +0700

    a

--- k/a.php
+++ k/a.php
@@ -2,13 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>งาน k --ธนชา พรหมบุตร(ต้นน้ำ)</title>
+    <title> งาน k --ธนชา พรหมบุตร(ต้นน้ำ)</title>
 </head>
 <body>
+
     <h1>ธนชา พรหมบุตร(ต้นน้ำ)</h1>
-    <button onclick="showimage('1.jpg', this)" style= "color:white; padding:10px; border:none;"><img src="1.jpg" width="150"></button>
-    <button onclick="showimage('1.jpg', this)" style= "color:white; padding:10px; border:none;"><img src="1.jpg" width="150"></button>
+
+ 
+    <button onclick="showImage()">
+        คลิกเพื่อแสดงรูป
+    </button>
+
+    <br><br>
+
     
+    <img id="display" width="300">
+
+    <script>
+        function showImage() {
+            document.getElementById("display").src = "1.jpg";
+        }
+    </script>
+
 </body>
 </html>
